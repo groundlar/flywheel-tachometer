d6035c4bf6717594fbc2070a5a279a84ade39f72
Skylar Cook <skylar.cook@noom.com>
2023-04-14 12:17:27 +0800

    run clang-format

--- lib/rpm_calculation.h
+++ lib/rpm_calculation.h
@@ -6,9 +6,11 @@
 
 namespace lib_rpm_calculation {
 
-uint32_t raw_to_millihertz(const std::vector<uint8_t>& readings, uint16_t sample_rate_hz);
-uint32_t raw_to_millirpm(const std::vector<uint8_t>& readings, uint16_t sample_rate_hz, uint16_t sensor_count=1);
+uint32_t raw_to_millihertz(const std::vector<uint8_t> &readings,
+                           uint16_t sample_rate_hz);
+uint32_t raw_to_millirpm(const std::vector<uint8_t> &readings,
+                         uint16_t sample_rate_hz, uint16_t sensor_count = 1);
 
-} // namespace lib_rpm_calculation 
+} // namespace lib_rpm_calculation
 
 #endif // LIB_RPM_CALCULATION_H_
--- lib/util.h
+++ lib/util.h
@@ -9,6 +9,6 @@ inline constexpr int32_t MICROS = 1000000;
 inline constexpr int32_t MILLIS = 1000;
 inline constexpr int32_t HERTZ_TO_RPM = 60;
 
-}
+} // namespace util
 
 #endif // LIB_UTIL_H_
