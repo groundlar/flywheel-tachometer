2465508e7af59b1efdc6a24be24cd115f2bf17a8
Skylar Cook <skylar.cook@noom.com>
2023-04-14 12:55:28 +0800

    fix pre-commit

--- pre-commit.sh
+++ pre-commit.sh
@@ -4,10 +4,7 @@
 # exit with non-zero status after issuing an appropriate message if
 # it wants to stop the commit.
 
-set -euo pipefail
-
-# If there are whitespace errors, print the offending file names and fail.
-exec git diff-index --check --cached $against --
+set -uo pipefail
 
 git commit --no-verify -m 'Save index'
 old_stash=$(git rev-parse -q --verify refs/stash)
@@ -15,10 +12,8 @@ git stash push -m 'Unstaged changes'
 new_stash=$(git rev-parse -q --verify refs/stash)
 git reset --soft HEAD^
 
-# format staged changes
-for file in `git diff --cached --name-only --diff-filter=ACMR`; do
-  clang-format -i "${file}"
-done
+# Format all files. No mercy.
+clang-format -i $(find . -type f -name "*.cc" -o -name "*.h")
 
 git add -u
 if [ "$old_stash" != "$new_stash" ]; then # if unstaged changes were stashed reapply to working tree
@@ -26,3 +21,12 @@ if [ "$old_stash" != "$new_stash" ]; then # if unstaged changes were stashed rea
 fi
 exit 0
 
+# If there are whitespace errors, print the offending file names and fail.
+if git rev-parse --verify HEAD >/dev/null 2>&1
+then
+	against=HEAD
+else
+	# Initial commit: diff against an empty tree object
+	against=$(git hash-object -t tree /dev/null)
+fi
+exec git diff-index --check --cached $against --
